26d8c130437d836823dceb4b5a5f349db5e1a915
Jens Nolte <git@queezle.net>
2021-03-27 21:54:09 +0100

    Merge example api definition into test case

--- test/Network/RpcSpec.hs
+++ test/Network/RpcSpec.hs
@@ -6,12 +6,31 @@ module Network.RpcSpec where
 import Prelude
 import Network.Rpc
 import Test.Hspec
-import Network.RpcSpec.ExampleApi
 
-
-$(makeProtocol exampleApi)
-$(makeClient exampleApi)
-$(makeServer exampleApi)
+$(do
+  let api = rpcApi "Example" [
+        rpcFunction "fixedHandler42" $ do
+          addArgument "arg" [t|Int|]
+          addResult "result" [t|Bool|]
+          setFixedHandler [| return . (== 42) |],
+        rpcFunction "fixedHandlerInc" $ do
+          addArgument "arg" [t|Int|]
+          addResult "result" [t|Int|]
+          setFixedHandler [| return . (+ 1) |],
+        rpcFunction "multiArgs" $ do
+          addArgument "one" [t|Int|]
+          addArgument "two" [t|Int|]
+          addArgument "three" [t|Bool|]
+          addResult "result" [t|Int|]
+          addResult "result2" [t|Bool|],
+        rpcFunction "noArgs" $ do
+          addResult "result" [t|Int|],
+        rpcFunction "noResponse" $ do
+          addArgument "arg" [t|Int|],
+        rpcFunction "noNothing" $ return ()
+        ]
+  mconcat <$> sequence [makeProtocol api, makeClient api, makeServer api]
+ )
 
 exampleProtocolImpl :: ExampleProtocolImpl
 exampleProtocolImpl = ExampleProtocolImpl {
