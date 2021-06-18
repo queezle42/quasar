f3f229c2b259c9eb0379ec6537463b080c6912ee
Jens Nolte <git@queezle.net>
2021-06-18 23:34:51 +0200

    Add helpers to calculate common unix socket locations

---
+++ src/Network/Rpc/SocketLocation.hs
@@ -0,0 +1,30 @@
+module Network.Rpc.SocketLocation where
+
+import Control.Exception (handle)
+import Prelude
+import System.Environment (getEnv)
+
+systemSocketPath :: String -> IO FilePath
+systemSocketPath appId = pure $ "/run/" <> appId
+
+userSocketPath :: String -> IO FilePath
+userSocketPath appId = do
+  xdgRuntimeDir <- getEnv "XDG_RUNTIME_DIR"
+  pure $ xdgRuntimeDir <> "/" <> appId
+
+sessionSocketPath :: String -> IO FilePath
+sessionSocketPath appId = do
+  waylandSocketPath' <- waylandSocketPath
+  maybe fallbackSocketPath pure waylandSocketPath'
+  where
+    waylandSocketPath :: IO (Maybe FilePath)
+    waylandSocketPath = handleEnvError $ do
+      xdgRuntimeDir <- getEnv "XDG_RUNTIME_DIR"
+      waylandDisplay <- getEnv "WAYLAND_DISPLAY"
+      pure $ xdgRuntimeDir <> "/" <> waylandDisplay <> "-" <> appId
+    fallbackSocketPath :: IO FilePath
+    fallbackSocketPath = do
+      xdgRuntimeDir <- getEnv "XDG_RUNTIME_DIR"
+      pure $ xdgRuntimeDir <> "/" <> appId
+    handleEnvError :: IO FilePath -> IO (Maybe FilePath)
+    handleEnvError = handle (const $ pure Nothing :: IOError -> IO (Maybe FilePath)) . fmap Just
