0d1aff4f6d57d45c58e42f212c073dce614ef78b
Jens Nolte <git@queezle.net>
2021-09-05 22:01:34 +0200

    Add Quasar module that re-exports core modules

---
+++ src/Quasar.hs
@@ -0,0 +1,13 @@
+module Quasar (
+  module Quasar.Async,
+  module Quasar.Awaitable,
+  module Quasar.Disposable,
+  module Quasar.Observable,
+  module Quasar.ResourceManager,
+) where
+
+import Quasar.Async
+import Quasar.Awaitable
+import Quasar.Disposable
+import Quasar.Observable
+import Quasar.ResourceManager
