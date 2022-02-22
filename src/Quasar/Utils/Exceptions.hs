9413c42765664a9712979335bbec7c940047a959
Jens Nolte <git@queezle.net>
2022-02-22 02:20:04 +0100

    Remove unnecessary bracket

--- src/Quasar/Utils/Exceptions.hs
+++ src/Quasar/Utils/Exceptions.hs
@@ -15,7 +15,7 @@ instance Exception CombinedException where
   displayException (CombinedException exceptions) = intercalate "\n" (header : exceptionMessages)
     where
       header = mconcat ["CombinedException with ", show (length exceptions), "exceptions:"]
-      exceptionMessages = (displayException <$> toList exceptions)
+      exceptionMessages = displayException <$> toList exceptions
 
 combinedExceptions :: CombinedException -> [SomeException]
 combinedExceptions (CombinedException exceptions) = toList exceptions
