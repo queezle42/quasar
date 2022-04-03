3d39af466f2a9fa3824d7cb06050e0cb754e9d99
Jens Nolte <git@queezle.net>
2022-04-03 03:00:40 +0200

    Export logger from Quasar module
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- src/Quasar.hs
+++ src/Quasar.hs
@@ -1,6 +1,7 @@
 module Quasar (
   module Quasar.Async,
   module Quasar.Future,
+  module Quasar.Logger,
   module Quasar.MonadQuasar,
   module Quasar.MonadQuasar.Misc,
   module Quasar.Observable,
@@ -9,6 +10,7 @@ module Quasar (
 
 import Quasar.Async
 import Quasar.Future
+import Quasar.Logger
 import Quasar.MonadQuasar
 import Quasar.MonadQuasar.Misc
 import Quasar.Observable
