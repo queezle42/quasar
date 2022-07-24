cd71ac69c35e1577438e73a5b5901572520b67b6
Jens Nolte <git@queezle.net>
2022-07-24 03:16:16 +0200

    Add ReaderLock
    Co-authored-by: Jan Beinke <git@janbeinke.com>

---
+++ src/Quasar/Utils/ReaderLock.hs
@@ -0,0 +1,63 @@
+module Quasar.Utils.ReaderLock (
+  ReaderLock,
+  newReaderLock,
+  newRecursiveReaderLock,
+  withReaderLock,
+  tryDestroyReaderLock,
+) where
+
+import Control.Monad.Catch
+import Quasar.Prelude
+
+data ReaderLockDestroyed = ReaderLockDestroyed
+  deriving stock Show
+  deriving anyclass Exception
+
+data ReaderLockState
+  = Valid Word64 (STM ()) (STM ())
+  | Destroyed
+
+data ReaderLock = ReaderLock (TVar ReaderLockState)
+
+newReaderLock :: STM () -> STM () -> STM ReaderLock
+newReaderLock lock unlock =
+  ReaderLock <$> newTVar (Valid 0 lock unlock)
+
+newRecursiveReaderLock :: ReaderLock -> STM ReaderLock
+newRecursiveReaderLock parent = newReaderLock lock unlock
+  where
+    lock = unsafeLockReaderLock parent
+    unlock = unsafeUnlockReaderLock parent
+
+unsafeLockReaderLock :: ReaderLock -> STM ()
+unsafeLockReaderLock (ReaderLock var) = do
+  readTVar var >>= \case
+    Valid counter lock unlock -> do
+      when (counter == 0) lock
+      writeTVar var $ Valid (counter + 1) lock unlock
+    Destroyed -> throwM ReaderLockDestroyed
+
+unsafeUnlockReaderLock :: ReaderLock -> STM ()
+unsafeUnlockReaderLock (ReaderLock var) = do
+  readTVar var >>= \case
+    Valid 1 _lock unlock -> do
+      unlock
+      writeTVar var Destroyed
+    Valid counter lock unlock ->
+      writeTVar var $ Valid (counter - 1) lock unlock
+    Destroyed -> throwM ReaderLockDestroyed
+
+tryDestroyReaderLock :: ReaderLock -> STM Bool
+tryDestroyReaderLock (ReaderLock var) =
+  stateTVar var
+    \case
+      Valid 0 _ _ -> (True, Destroyed)
+      Destroyed -> (True, Destroyed)
+      state -> (False, state)
+
+withReaderLock :: (MonadIO m, MonadMask m) => ReaderLock -> m a -> m a
+withReaderLock cl fn =
+  bracket
+    (atomically $ unsafeLockReaderLock cl)
+    (\() -> atomically $ unsafeUnlockReaderLock cl)
+    (\() -> fn)
