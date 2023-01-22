15249383f55370ecb983f0ab6be6057a93e7d6d6
Jens Nolte <git@queezle.net>
2023-01-22 23:50:32 +0100

    Add partial implementation of ObservableMap delta updates
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- quasar/src/Quasar/Observable/ObservableMap.hs
+++ quasar/src/Quasar/Observable/ObservableMap.hs
@@ -9,21 +9,25 @@ module Quasar.Observable.ObservableMap (
   lookup,
 ) where
 
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
 import Quasar.Observable
 import Quasar.Observable.Internal.ObserverRegistry
 import Quasar.Prelude
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as Map
+import Quasar.Resources.Disposer
 
 
 class IsObservable (Map k v) a => IsObservableMap k v a where
   observeKey :: Ord k => k -> a -> Observable (Maybe v)
   observeKey key = observeKey key . toObservableMap
 
+  attachUpdateListener :: a -> (ObservableMapUpdate k v -> STMc NoRetry '[] ()) -> STMc NoRetry '[] TSimpleDisposer
+  attachUpdateListener x = attachUpdateListener (toObservableMap x)
+
   toObservableMap :: a -> ObservableMap k v
   toObservableMap = ObservableMap
 
-  {-# MINIMAL toObservableMap | observeKey #-}
+  {-# MINIMAL toObservableMap | observeKey, attachUpdateListener #-}
 
 
 data ObservableMap k v = forall a. IsObservableMap k v a => ObservableMap a
@@ -39,6 +43,20 @@ instance Functor (ObservableMap k) where
   fmap f x = toObservableMap (MappedObservableMap f x)
 
 
+type ObservableMapUpdate k v = [ObservableMapDelta k v]
+
+data ObservableMapDelta k v = Reset | Insert k v | Delete k | Rename k k
+
+instance Functor (ObservableMapDelta k) where
+  fmap _ Reset = Reset
+  fmap f (Insert k v) = Insert k (f v)
+  fmap _ (Delete k) = Delete k
+  fmap _ (Rename k0 k1) = Rename k0 k1
+
+initialObservableMapDelta :: Map k v -> [ObservableMapDelta k v]
+initialObservableMapDelta = fmap (\(k, v) -> Insert k v) . Map.toList
+
+
 data MappedObservableMap k v = forall a. MappedObservableMap (a -> v) (ObservableMap k a)
 
 instance IsObservable (Map k v) (MappedObservableMap k v) where
@@ -46,11 +64,13 @@ instance IsObservable (Map k v) (MappedObservableMap k v) where
 
 instance IsObservableMap k v (MappedObservableMap k v) where
   observeKey key (MappedObservableMap fn observable) = fn <<$>> observeKey key observable
-
+  attachUpdateListener (MappedObservableMap fn observable) callback =
+    attachUpdateListener observable (\update -> callback (fn <<$>> update))
 
 
 data ObservableMapVar k v = ObservableMapVar {
   content :: ObservableVar (Map k v),
+  deltaListeners :: ObserverRegistry [ObservableMapDelta k v],
   keyObservers :: TVar (Map k (ObserverRegistry (Maybe v)))
 }
 
@@ -59,6 +79,9 @@ instance IsObservable (Map k v) (ObservableMapVar k v) where
 
 instance IsObservableMap k v (ObservableMapVar k v) where
   observeKey key x = toObservable (ObservableMapVarKeyObservable key x)
+  attachUpdateListener ObservableMapVar{content, deltaListeners} callback = do
+    initial <- initialObservableMapDelta <$> readObservable content
+    registerObserver deltaListeners callback initial
 
 
 data ObservableMapVarKeyObservable k v = ObservableMapVarKeyObservable k (ObservableMapVar k v)
@@ -80,8 +103,9 @@ instance Ord k => IsObservable (Maybe (v)) (ObservableMapVarKeyObservable k v) w
 new :: MonadSTMc NoRetry '[] m => m (ObservableMapVar k v)
 new = liftSTMc @NoRetry @'[] do
   content <- newObservableVar Map.empty
+  deltaListeners <- newObserverRegistry
   keyObservers <- newTVar Map.empty
-  pure ObservableMapVar {content, keyObservers}
+  pure ObservableMapVar {content, deltaListeners, keyObservers}
 
 insert :: forall k v m. (Ord k, MonadSTMc NoRetry '[] m) => k -> v -> ObservableMapVar k v -> m ()
 insert key value ObservableMapVar{content, keyObservers} = liftSTMc @NoRetry @'[] do
