55fbe088fd50dc32f44a0957cdc2b7d72898a159
Jens Nolte <git@queezle.net>
2023-01-29 00:27:32 +0100

    Prepare TOnce for change of IsFuture
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- quasar/src/Quasar/Utils/TOnce.hs
+++ quasar/src/Quasar/Utils/TOnce.hs
@@ -4,6 +4,7 @@ module Quasar.Utils.TOnce (
   newTOnceIO,
   mapFinalizeTOnce,
   finalizeTOnce,
+  readTOnce,
 
   -- * Exceptions
   TOnceAlreadyFinalized,
@@ -16,32 +17,36 @@ data TOnceAlreadyFinalized = TOnceAlreadyFinalized
   deriving stock (Eq, Show)
   deriving anyclass Exception
 
-data TOnce a b = TOnce (TVar (Maybe a)) (Promise b)
+data TOnce a b = TOnce (TVar (Either a b))
 
 instance IsFuture b (TOnce a b) where
-  toFuture (TOnce _ promise) = toFuture promise
+  --toFuture (TOnce _ promise) = toFuture promise
+  toFuture = undefined
 
 newTOnce :: MonadSTMc NoRetry '[] m => a -> m (TOnce a b)
-newTOnce initial = TOnce <$> newTVar (Just initial) <*> newPromise
+newTOnce initial = TOnce <$> newTVar (Left initial)
 
 newTOnceIO :: MonadIO m => a -> m (TOnce a b)
-newTOnceIO initial = TOnce <$> newTVarIO (Just initial) <*> newPromiseIO
+newTOnceIO initial = TOnce <$> newTVarIO (Left initial)
 
 
+--mapFinalizeTOnce :: MonadSTMc NoRetry '[] m => TOnce a (Future b) -> (a -> m b) -> m (Future b)
 mapFinalizeTOnce :: MonadSTMc NoRetry '[] m => TOnce a b -> (a -> m b) -> m (Future b)
-mapFinalizeTOnce (TOnce var promise) fn = do
-  readTVar var >>= \case
-    Just initial -> do
-      writeTVar var Nothing
-      final <- fn initial
-      tryFulfillPromise_ promise final
-      pure (toFuture promise)
-    Nothing -> pure (toFuture promise)
+mapFinalizeTOnce = undefined
+--mapFinalizeTOnce (TOnce var) fn = do
+--  readTVar var >>= \case
+--    Just initial -> do
+--      writeTVar var Nothing
+--      final <- fn initial
+--      tryFulfillPromise_ promise final
+--      pure (toFuture promise)
+--    Nothing -> pure (toFuture promise)
 
 finalizeTOnce :: MonadSTMc NoRetry '[TOnceAlreadyFinalized] m => TOnce a b -> b -> m ()
-finalizeTOnce (TOnce var promise) value =
+finalizeTOnce (TOnce var) value =
   readTVar var >>= \case
-    Just _ -> do
-      writeTVar var Nothing
-      tryFulfillPromise_ promise value
-    Nothing -> throwC TOnceAlreadyFinalized
+    Left _ -> writeTVar var (Right value)
+    Right _ -> throwC TOnceAlreadyFinalized
+
+readTOnce :: MonadSTMc NoRetry '[] m => TOnce a b -> m (Either a b)
+readTOnce (TOnce var) = readTVar var
