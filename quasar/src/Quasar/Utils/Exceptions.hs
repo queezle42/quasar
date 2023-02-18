e87646c3351c2a3412bba78f43d92e5ac671dfcb
Jens Nolte <git@queezle.net>
2023-02-18 23:34:36 +0100

    Add mkCombinedException
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- quasar/src/Quasar/Utils/Exceptions.hs
+++ quasar/src/Quasar/Utils/Exceptions.hs
@@ -1,15 +1,19 @@
 module Quasar.Utils.Exceptions (
   CombinedException(..),
+  mkCombinedException,
   combinedExceptions,
 ) where
 
 import Control.Exception
 import Data.Foldable (toList)
-import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty (NonEmpty(..), nonEmpty)
 import Quasar.Prelude
 
 newtype CombinedException = CombinedException (NonEmpty SomeException)
   deriving stock Show
+
+mkCombinedException :: [SomeException] -> Maybe CombinedException
+mkCombinedException exs = CombinedException <$> nonEmpty exs
 
 instance Exception CombinedException where
   displayException (CombinedException exceptions) = intercalate "\n" (header : exceptionMessages)
