0fd6ee7fadf58e3ac6d215c92608fec93c31b436
Jens Nolte <git@queezle.net>
2023-02-26 23:01:26 +0100

    Split IsObservableMap and ToObservableMap classes
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- quasar/src/Quasar/Observable/ObservableMap.hs
+++ quasar/src/Quasar/Observable/ObservableMap.hs
@@ -1,4 +1,7 @@
 module Quasar.Observable.ObservableMap (
+  ToObservableMap(..),
+  observeKey,
+  attachDeltaObserver,
   IsObservableMap(..),
   ObservableMap,
 
@@ -25,30 +28,40 @@ import Quasar.Utils.CallbackRegistry
 import Quasar.Utils.Map qualified as Map
 
 
-class ToObservable (Map k v) a => IsObservableMap k v a where
-  {-# MINIMAL toObservableMap | observeKey, attachDeltaObserver #-}
+class ToObservable (Map k v) a => ToObservableMap k v a where
+  toObservableMap :: a -> ObservableMap k v
+  default toObservableMap :: IsObservableMap k v a => a -> ObservableMap k v
+  toObservableMap = ObservableMap
 
-  observeKey :: Ord k => k -> a -> Observable (Maybe v)
-  observeKey key = observeKey key . toObservableMap
+class ToObservableMap k v a => IsObservableMap k v a where
+  {-# MINIMAL observeKey#, attachDeltaObserver# #-}
+
+  observeKey# :: Ord k => k -> a -> Observable (Maybe v)
+  observeKey# key = observeKey# key . toObservableMap
 
   -- | Register a listener to observe changes to the whole map. The callback
   -- will be invoked with the current state of the map immediately after
   -- registering and after that will be invoked for every change to the map.
-  attachDeltaObserver :: a -> (ObservableMapDelta k v -> STMc NoRetry '[] ()) -> STMc NoRetry '[] TSimpleDisposer
-  attachDeltaObserver x = attachDeltaObserver (toObservableMap x)
+  attachDeltaObserver# :: a -> (ObservableMapDelta k v -> STMc NoRetry '[] ()) -> STMc NoRetry '[] TSimpleDisposer
+  attachDeltaObserver# x = attachDeltaObserver# (toObservableMap x)
 
-  toObservableMap :: a -> ObservableMap k v
-  toObservableMap = ObservableMap
+observeKey :: (ToObservableMap k v a, Ord k) => k -> a -> Observable (Maybe v)
+observeKey key x = observeKey# key (toObservableMap x)
 
+attachDeltaObserver :: (ToObservableMap k v a, MonadSTMc NoRetry '[] m) => a -> (ObservableMapDelta k v -> STMc NoRetry '[] ()) -> m TSimpleDisposer
+attachDeltaObserver x callback = liftSTMc $ attachDeltaObserver# (toObservableMap x) callback
 
 data ObservableMap k v = forall a. IsObservableMap k v a => ObservableMap a
 
 instance ToObservable (Map k v) (ObservableMap k v) where
   toObservable (ObservableMap x) = toObservable x
 
-instance IsObservableMap k v (ObservableMap k v) where
-  observeKey key (ObservableMap x) = observeKey key x
+instance ToObservableMap k v (ObservableMap k v) where
   toObservableMap = id
+
+instance IsObservableMap k v (ObservableMap k v) where
+  observeKey# key (ObservableMap x) = observeKey# key x
+  attachDeltaObserver# (ObservableMap x) = attachDeltaObserver# x
 
 instance Functor (ObservableMap k) where
   fmap f x = toObservableMap (MappedObservableMap f x)
@@ -81,10 +94,12 @@ data MappedObservableMap k v = forall a. MappedObservableMap (a -> v) (Observabl
 instance ToObservable (Map k v) (MappedObservableMap k v) where
   toObservable (MappedObservableMap fn observable) = fn <<$>> toObservable observable
 
+instance ToObservableMap k v (MappedObservableMap k v)
+
 instance IsObservableMap k v (MappedObservableMap k v) where
-  observeKey key (MappedObservableMap fn observable) = fn <<$>> observeKey key observable
-  attachDeltaObserver (MappedObservableMap fn observable) callback =
-    attachDeltaObserver observable (\update -> callback (fn <<$>> update))
+  observeKey# key (MappedObservableMap fn observable) = fn <<$>> observeKey# key observable
+  attachDeltaObserver# (MappedObservableMap fn observable) callback =
+    attachDeltaObserver# observable (\update -> callback (fn <<$>> update))
 
 
 data ObservableMapVar k v = ObservableMapVar {
@@ -103,9 +118,11 @@ instance IsObservable (Map k v) (ObservableMapVar k v) where
     value <- readTVar content
     pure (disposer, value)
 
+instance ToObservableMap k v (ObservableMapVar k v)
+
 instance IsObservableMap k v (ObservableMapVar k v) where
-  observeKey key x = toObservable (ObservableMapVarKeyObservable key x)
-  attachDeltaObserver ObservableMapVar{content, deltaObservers} callback = do
+  observeKey# key x = toObservable (ObservableMapVarKeyObservable key x)
+  attachDeltaObserver# ObservableMapVar{content, deltaObservers} callback = do
     disposer <- registerCallback deltaObservers callback
     callback =<< initialObservableMapDelta <$> readTVar content
     pure disposer
@@ -171,12 +188,14 @@ instance ToObservable (Map k v) (FilteredObservableMap k v) where
   toObservable (FilteredObservableMap predicate upstream) =
     mapObservable (Map.filter predicate) upstream
 
-instance IsObservableMap k v (FilteredObservableMap k v) where
-  observeKey key (FilteredObservableMap predicate upstream) =
-    find predicate <$> observeKey key upstream
+instance ToObservableMap k v (FilteredObservableMap k v)
 
-  attachDeltaObserver (FilteredObservableMap predicate upstream) callback =
-    attachDeltaObserver upstream \delta -> callback (filterDelta <$> delta)
+instance IsObservableMap k v (FilteredObservableMap k v) where
+  observeKey# key (FilteredObservableMap predicate upstream) =
+    find predicate <$> observeKey# key upstream
+
+  attachDeltaObserver# (FilteredObservableMap predicate upstream) callback =
+    attachDeltaObserver# upstream \delta -> callback (filterDelta <$> delta)
     where
       filterDelta :: ObservableMapOperation k v -> ObservableMapOperation k v
       filterDelta (Insert key value) =
