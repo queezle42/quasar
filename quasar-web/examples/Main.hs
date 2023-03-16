d6cc4a6a864dd281637b0e90ddb75b04225a38e0
Jens Nolte <git@queezle.net>
2023-03-16 23:31:30 +0100

    Expand example

--- quasar-web/examples/Main.hs
+++ quasar-web/examples/Main.hs
@@ -1,13 +1,31 @@
 module Main (main) where
 
+import Control.Concurrent (forkIO, threadDelay)
 import Network.Wai.Handler.Warp
 import Quasar.Prelude
+import Quasar
 import Quasar.Web
 import Quasar.Web.Server (waiApplication)
 import System.IO (hPutStrLn, stderr)
+import Data.ByteString.Lazy.UTF8 qualified as UTF8
 
 main :: IO ()
-main = runSettings settings (waiApplication (WebUiObservable (pure (WebUiHtmlElement (HtmlElement "<h1>Hello World!</h1>")))))
+main = do
+  a <- newObservableVarIO (1 :: Int)
+  b <- newObservableVarIO (0 :: Int)
+
+  void $ forkIO $ forever do
+    v <- atomically $ stateObservableVar a (\x -> (x, x + 1))
+    when (v `mod` 10 == 0) do
+      atomically $ modifyObservableVar b (+ 10)
+    threadDelay 1_000_000
+
+  runSettings settings $ waiApplication do
+    WebUiConcat [
+      WebUiObservable (toSpan <$> toObservable a),
+      WebUiHtmlElement (HtmlElement "/"),
+      WebUiObservable (toSpan <$> toObservable b)
+      ]
   where
     port :: Port
     port = 9013
@@ -15,3 +33,6 @@ main = runSettings settings (waiApplication (WebUiObservable (pure (WebUiHtmlEle
       (setBeforeMainLoop (hPutStrLn stderr ("Listening on port " <> show port))) $
       (setPort port)
       defaultSettings
+
+toSpan :: Show a => a -> WebUi
+toSpan x = WebUiHtmlElement (HtmlElement (mconcat ["<span>", UTF8.fromString (show x), "</span>"]))
