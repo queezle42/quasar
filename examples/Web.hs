b84a771de80479c4b02964c8d273a35ee9f5682e
Jens Nolte <git@queezle.net>
2023-03-28 02:47:18 +0200

    examples: Use WebUiObservableList in quasar-web-example

--- examples/Web.hs
+++ examples/Web.hs
@@ -4,32 +4,46 @@ import Control.Concurrent (forkIO, threadDelay)
 import Data.Text.Lazy qualified as TL
 import Network.Wai.Handler.Warp
 import Quasar
+import Quasar.Observable.ObservableMap as ObservableMap
 import Quasar.Prelude
 import Quasar.Web
 import Quasar.Web.Server (waiApplication)
 import System.IO (hPutStrLn, stderr)
+import System.Random
 
 main :: IO ()
 main = do
   a <- newObservableVarIO (0 :: Int)
   b <- newObservableVarIO (0 :: Int)
-  c <- newObservableVarIO undefined
+  c <- newObservableVarIO (WebUiConcat [])
+  d <- newObservableMapVarIO
 
   void $ forkIO $ forever do
     atomically do
       v <- stateObservableVar a (\x -> (x + 1, x + 1))
       when (v `mod` 10 == 0) do
         modifyObservableVar b (+ 10)
-      when (v `mod` 7 == 0) do
+      when (v `mod` 23 == 0) do
         writeObservableVar c (WebUiHtmlElement (HtmlElement "🐿"))
-      when (v `mod` 7 == 1) do
+      when (v `mod` 23 == 1) do
         writeObservableVar c do
           WebUiConcat [
             WebUiObservable (toSpan <$> toObservable a),
             WebUiHtmlElement (HtmlElement "/"),
-            WebUiObservable (toSpan <$> toObservable b)
+            WebUiObservable (toSpan <$> toObservable b),
+            WebUiObservableList (ObservableMap.values d)
             ]
     threadDelay 1_000_000
+
+  void $ forkIO $ forever do
+    x <- randomRIO @Int (0, 100)
+    atomically do
+      ObservableMap.insert x (WebUiHtmlElement (HtmlElement ("<div>" <> TL.pack (show x) <> "</div>"))) d
+    threadDelay 1_000_000
+    y <- randomRIO @Int (0, 100)
+    atomically do
+      ObservableMap.delete y d
+    threadDelay 700_000
 
   runSettings settings $ waiApplication do
     WebUiObservable (toObservable c)
@@ -37,8 +51,8 @@ main = do
     port :: Port
     port = 9013
     settings =
-      (setBeforeMainLoop (hPutStrLn stderr ("Listening on port " <> show port))) $
-      (setPort port)
+      setBeforeMainLoop (hPutStrLn stderr ("Listening on port " <> show port)) $
+      setPort port
       defaultSettings
 
 toSpan :: Show a => a -> WebUi
