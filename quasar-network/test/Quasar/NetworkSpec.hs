c638c42dd9d561e86f62a192d4c233c8137fa93b
Jens Nolte <git@queezle.net>
2023-04-23 21:25:26 +0200

    quasar-network: Add tests for NetworkFunction instances

---
+++ quasar-network/test/Quasar/NetworkSpec.hs
@@ -0,0 +1,41 @@
+module Quasar.NetworkSpec (spec) where
+
+import Quasar
+import Quasar.Network.Client
+import Quasar.Prelude
+import System.Timeout (timeout)
+import Test.Hspec.Core.Spec
+
+-- Type is pinned to IO, otherwise hspec spec type cannot be inferred
+rm :: QuasarIO a -> IO a
+rm = runQuasarCombineExceptions
+
+testTimeout :: Int -> IO () -> IO ()
+testTimeout time fn =
+  timeout time fn >>= \case
+    Nothing -> fail $ mconcat ["Test reached timeout (", show time, "ns)"]
+    Just () -> pure ()
+
+spec :: Spec
+spec = parallel $ describe "NetworkFunction" $ do
+  it "can call a proxy function" $ testTimeout 1_000_000 $ rm do
+    let
+      theFunction  :: IO (Future Int)
+      theFunction = do
+        pure (pure 42)
+
+    42 <- withStandaloneProxy theFunction \proxy -> do
+      await =<< liftIO proxy
+
+    pure ()
+
+  it "can call a proxy function with an argument" $ testTimeout 1_000_000 $ rm do
+    let
+      theFunction  :: Int -> IO (Future Int)
+      theFunction arg = do
+        pure (pure (arg * 2))
+
+    42 <- withStandaloneProxy theFunction \proxy -> do
+      await =<< liftIO (proxy 21)
+
+    pure ()
