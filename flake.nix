c8dc1c25270c2beb52dabee1f87e17e09d96d5f7
Jens Nolte <git@queezle.net>
2023-05-21 05:09:20 +0200

    Add cachix integration

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
 {
+  nixConfig.extra-substituters = [ "https://quasar.cachix.org" ];
+  nixConfig.extra-trusted-public-keys = [ "quasar.cachix.org-1:dYbJvSTcYrGGKuoaoQe3Fhomv9LrpUG1fepNtffRex4=" ];
+
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   };
