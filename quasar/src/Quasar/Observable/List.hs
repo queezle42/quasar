6506668e0eb9eaa59fbc09a45cefbe1722ae0dcb
Jens Nolte <git@queezle.net>
2023-07-05 05:49:10 +0200

    quasar: Change new ObservableList container type to Seq
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- quasar/src/Quasar/Observable/List.hs
+++ quasar/src/Quasar/Observable/List.hs
@@ -9,13 +9,15 @@ module Quasar.Observable.List (
 ) where
 
 import Data.Binary (Binary)
+import Data.Sequence (Seq(Empty, (:<|), (:|>)))
+import Data.Sequence qualified as Seq
 import Quasar.Observable.Core
 import Quasar.Prelude
 
 
 data ObservableListDelta v
-  = ObservableListUpdate [ObservableListOperation v]
-  | ObservableListReplace [v]
+  = ObservableListUpdate (Seq (ObservableListOperation v))
+  | ObservableListReplace (Seq v)
   deriving Generic
 
 instance Binary v => Binary (ObservableListDelta v)
@@ -25,36 +27,39 @@ data ObservableListOperation v
 
 instance Binary v => Binary (ObservableListOperation v)
 
-instance ObservableContainer [] v where
-  type ContainerConstraint canLoad exceptions [] v a = IsObservableList canLoad exceptions v a
-  type Delta [] = ObservableListDelta
-  type EvaluatedDelta [] v = (ObservableListDelta v, [v])
-  type Key [] v = Int
+instance ObservableContainer Seq v where
+  type ContainerConstraint canLoad exceptions Seq v a = IsObservableList canLoad exceptions v a
+  type Delta Seq = ObservableListDelta
+  type EvaluatedDelta Seq v = (ObservableListDelta v, Seq v)
+  type Key Seq v = Int
+  type DeltaContext Seq v = Word32
   applyDelta _delta _state = undefined
   mergeDelta _old _new = undefined
+  updateDeltaContext = undefined
   toInitialDelta = undefined
+  toInitialDeltaContext = undefined
   initializeFromDelta = undefined
   toDelta = fst
   toEvaluatedDelta = (,)
   toEvaluatedContent = snd
 
-instance ContainerCount [] where
+instance ContainerCount Seq where
   containerCount# x = fromIntegral (length x)
   containerIsEmpty# x = null x
 
 
-type ToObservableList canLoad exceptions v a = ToObservableT canLoad exceptions [] v a
+type ToObservableList canLoad exceptions v a = ToObservableT canLoad exceptions Seq v a
 
 toObservableList :: ToObservableList canLoad exceptions v a => a -> ObservableList canLoad exceptions v
 toObservableList x = ObservableList (toObservableCore x)
 
 newtype ObservableList canLoad exceptions v
-  = ObservableList (ObservableT canLoad exceptions [] v)
+  = ObservableList (ObservableT canLoad exceptions Seq v)
 
-instance ToObservableT canLoad exceptions [] v (ObservableList canLoad exceptions v) where
+instance ToObservableT canLoad exceptions Seq v (ObservableList canLoad exceptions v) where
   toObservableCore (ObservableList x) = x
 
-instance IsObservableCore canLoad exceptions [] v (ObservableList canLoad exceptions v) where
+instance IsObservableCore canLoad exceptions Seq v (ObservableList canLoad exceptions v) where
   readObservable# (ObservableList x) = readObservable# x
   attachObserver# (ObservableList x) = attachObserver# x
   attachEvaluatedObserver# (ObservableList x) = attachEvaluatedObserver# x
@@ -65,7 +70,7 @@ instance IsObservableList canLoad exceptions v (ObservableList canLoad exception
   --listLookupValue# (ObservableList x) = listLookupValue# x
 
 
-class IsObservableCore canLoad exceptions [] v a => IsObservableList canLoad exceptions v a where
+class IsObservableCore canLoad exceptions Seq v a => IsObservableList canLoad exceptions v a where
   member# :: Ord v => a -> v -> Observable canLoad exceptions Bool
   member# = undefined
 
@@ -76,4 +81,4 @@ class IsObservableCore canLoad exceptions [] v a => IsObservableList canLoad exc
   query# = undefined
 
 
-instance IsObservableList canLoad exceptions v (ObservableState canLoad (ObservableResult exceptions []) v) where
+instance IsObservableList canLoad exceptions v (ObservableState canLoad (ObservableResult exceptions Seq) v) where
