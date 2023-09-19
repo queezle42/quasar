f57e4a5628d1f61ed2a4f2ef4457e7bd76b55d5d
Jens Nolte <git@queezle.net>
2023-09-20 01:58:23 +0200

    quasar-web: Update dom types to more closely match browser dom
    Co-authored-by: Jan Beinke <git@janbeinke.com>

--- quasar-web/src/Quasar/Web.hs
+++ quasar-web/src/Quasar/Web.hs
@@ -1,6 +1,9 @@
 module Quasar.Web (
+  DomNode(..),
   DomElement(..),
-  DomElementContent(..),
+  domElement,
+  DomTextNode(..),
+  textNode,
 
   WebUi(..),
   HtmlElement(..),
@@ -9,19 +12,42 @@ module Quasar.Web (
 import Data.Text (Text)
 import Data.Text.Lazy qualified as TL
 import Quasar
-import Quasar.Observable.ObservableList
-import Quasar.Observable.ObservableMap
+import Quasar.Observable.Core
+import Quasar.Observable.List
+import Quasar.Observable.Map
 import Quasar.Prelude
+import Data.String (IsString(..))
+
+data DomNode
+  = DomNodeElement DomElement
+  | DomNodeTextNode DomTextNode
+
+instance IsString DomNode where
+  fromString x = DomNodeTextNode (fromString x)
 
 data DomElement = DomElement {
   tagName :: Text,
-  attributes :: ObservableMap Text (Maybe Text),
-  content :: DomElementContent
+  attributes :: ObservableMap NoLoad '[] Text (Maybe Text),
+  children :: ObservableList NoLoad '[] DomNode
 }
 
-data DomElementContent
---  = Children (ObservableList DomElement)
---  | InnerText (Observable Text)
+newtype DomTextNode
+  = DomTextNode (Observable NoLoad '[] Text)
+
+instance IsString DomTextNode where
+  fromString x = DomTextNode (fromString x)
+
+
+domElement ::
+  Text ->
+  ObservableMap NoLoad '[] Text (Maybe Text) ->
+  ObservableList NoLoad '[] DomNode ->
+  DomNode
+domElement tag attributes children = DomNodeElement (DomElement tag attributes children)
+
+textNode :: Observable NoLoad '[] Text -> DomNode
+textNode text = DomNodeTextNode (DomTextNode text)
+
 
 
 newtype HtmlElement = HtmlElement TL.Text
