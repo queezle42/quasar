3efe93ae34627c19df20e10ccad88aa14c6c1e3e
Jens Nolte <git@queezle.net>
2024-04-05 23:04:59 +0200

    quasar: Replace RcVar with Rc, with tryExtractRc function
    Extracting the value from an RcVar was always planned, but having an
    option to not call the cleanup function as part of the RcVar api will
    lead to problems in use-cases where that's illegal.
    
    To prevent future issues, the Rc type (previously RcVar) is now
    readonly. The cleanup function has been removed, the content is required
    to be disposable. This allows to dispose the content after using
    `tryExtractRc`.
    
    If required, a mutable variant can be added back later (without the
    option to extract the content).

---
+++ quasar/src/Quasar/Resources/Rc.hs
@@ -0,0 +1,97 @@
+module Quasar.Resources.Rc (
+  Rc,
+  newRc,
+  newRcIO,
+  tryReadRc,
+  tryDuplicateRc,
+  tryExtractRc,
+) where
+
+import Quasar.Prelude
+import Quasar.Resources
+import Quasar.Resources.DisposableVar
+import Quasar.Exceptions (ExceptionSink)
+
+-- | A Rc is a disposable readonly data structure that can be cloned. Every copy
+-- has an independent lifetime. The content is disposed when all copies of the
+-- Rc are disposed.
+newtype Rc a = Rc (DisposableVar (RcRc a))
+  deriving Disposable
+
+data RcRc a = RcRc {
+  -- Refcount that tracks how many locks exists in this group of locks.
+  lockCount :: TVar Word64,
+  disposer :: Disposer,
+  content :: a
+}
+
+decrementRc :: RcRc a -> STMc NoRetry '[] Disposer
+decrementRc rc = do
+  let lockCount = rc.lockCount
+  c <- readTVar lockCount
+  case c of
+    -- Special case - when called from `tryExtractRc` we should not run the
+    -- cleanup function
+    0 -> pure mempty
+
+    -- Last owner disposed, run cleanup
+    1 -> do
+      writeTVar rc.lockCount 0
+      pure rc.disposer
+
+    -- Decrement rc count
+    _ -> mempty <$ writeTVar rc.lockCount (pred c)
+
+-- | Extract the content of an Rc without disposing the content. This only has
+-- an effect if there are no other vars in the same group.
+tryExtractRc :: Rc a -> STMc NoRetry '[] (Maybe a)
+tryExtractRc (Rc var) = do
+  tryReadDisposableVar var >>= \case
+    Nothing -> pure Nothing
+    Just rc -> do
+      c <- readTVar rc.lockCount
+      if c == 1
+        then do
+          -- Set count to 0, which prevents the cleanup function from running
+          writeTVar rc.lockCount 0
+          -- Dispose DisposableVar to make content unavailable through the Rc
+          disposeEventually# var
+          pure (Just rc.content)
+
+        else pure Nothing
+
+newRc :: (Disposable a, MonadSTMc NoRetry '[] m) => a -> m (Rc a)
+newRc content = liftSTMc @NoRetry @'[] do
+  lockCount <- newTVar 1
+  let rc = RcRc {
+    lockCount,
+    disposer = getDisposer content,
+    content
+  }
+  Rc <$> newDisposableVar decrementRc rc
+
+newRcIO :: (Disposable a, MonadIO m) => a -> m (Rc a)
+newRcIO content = liftIO do
+  lockCount <- newTVarIO 1
+  let rc = RcRc {
+    lockCount,
+    disposer = getDisposer content,
+    content
+  }
+  Rc <$> newDisposableVarIO decrementRc rc
+
+-- | Read the content of the lock, if the lock has not been disposed.
+tryReadRc :: MonadSTMc NoRetry '[] m => Rc a -> m (Maybe a)
+tryReadRc (Rc var) = liftSTMc @NoRetry @'[] do
+  (.content) <<$>> tryReadDisposableVar var
+
+-- | Produces a _new_ lock that points to the same content, but has an
+-- independent lifetime. The caller has to ensure the new lock is disposed.
+--
+-- Usually this would be used to pass a copy of the lock to another component.
+tryDuplicateRc :: MonadSTMc NoRetry '[] m => Rc a -> m (Maybe (Rc a))
+tryDuplicateRc (Rc var) = liftSTMc @NoRetry @'[] do
+  tryReadDisposableVar var >>= mapM \rc -> do
+    modifyTVar rc.lockCount succ
+    Rc <$> newDisposableVar decrementRc rc
+
